4605fc25d1f85ab4ae12f4fe7fd52e9090d6e6be
VoTrieuAn <voan2068@gmail.com>
2025-02-27 13:40:22 +0700

    Fix error 'missing suspense boundary with usesearchparams' by nextJS

--- src/app/(pages)/search/page.tsx
+++ src/app/(pages)/search/page.tsx
@@ -1,6 +1,7 @@
 import { SearchItem } from "@/app/components/MusicApp/Search/SearchItem";
 import { Title } from "@/app/components/Title/Title";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Kết quả tìm kiếm bài hát",
@@ -15,7 +16,9 @@ export default function SearchPage() {
         <Title text="Kết quả tìm kiếm" />
         <div className="grid grid-cols-1 gap-[10px]">
           {/* Song Item Play */}
-          <SearchItem />
+          <Suspense>
+            <SearchItem />
+          </Suspense>
           {/* End Song Item Play */}
         </div>
       </div>
